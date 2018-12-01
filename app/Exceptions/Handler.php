80a552d10550a356c2002c70cc45d304ef9a49b6
developersajib <sajibwebdeveloper@gmail.com>
2018-12-01 11:48:40 +0600

    JWT Middleware, JWT Exception

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,11 @@ namespace App\Exceptions;
 
 use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Exceptions\TokenInvalidException;
+use Tymon\JWTAuth\Exceptions\TokenBlacklistedException;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
+use Illuminate\Http\Response;
 
 class Handler extends ExceptionHandler
 {
@@ -44,8 +49,27 @@ class Handler extends ExceptionHandler
      * @param  \Exception  $exception
      * @return \Illuminate\Http\Response
      */
-    public function render($request, Exception $exception)
-    {
+    public function render($request, Exception $exception )
+    {   
+        
+        if($exception instanceOf  TokenBlacklistedException){
+            return response(['error' => 'Token is not be used, get new one'], Response::HTTP_BAD_REQUEST);
+            
+        } 
+        elseif($exception instanceOf TokenInvalidException){
+            return response(['error' => 'Token is not valid'], Response::HTTP_BAD_REQUEST);
+        }
+
+        elseif($exception instanceOf TokenExpiredException){
+            return response(['error' => 'Token is expired'], Response::HTTP_BAD_REQUEST);
+        }
+
+        elseif($exception instanceOf JWTException){
+            return response(['error' => 'Token is not provided'], Response::HTTP_BAD_REQUEST);
+        }
+
+        
+
         return parent::render($request, $exception);
     }
 }
--- app/Http/Controllers/ReplyController.php
+++ app/Http/Controllers/ReplyController.php
@@ -9,6 +9,21 @@ use App\Model\Question;
 use App\Http\Resources\ReplyResource;
 class ReplyController extends Controller
 {
+    
+
+    /**
+     * Create a new AuthController instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('JWT', ['except' => ['index', 'show']]);
+    }
+
+
+
+
     /**
      * Display a listing of the resource.
      *
